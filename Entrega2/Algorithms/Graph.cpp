48f4e32e1b50a1025940537cf98b8df9a92c89a2
marhcouto <marhcouto1101@gmail.com>
2021-05-09 23:02:11 +0100

    Started

--- Entrega2/Algorithms/Graph.cpp
+++ Entrega2/Algorithms/Graph.cpp
@@ -3,3 +3,150 @@
 //
 
 #include "Graph.h"
+
+
+// N O D E
+
+template <class T>
+Vertex<T>::Vertex(T in): info(in) {}
+
+template <class T>
+Edge<T> *Vertex<T>::addEdge(Vertex<T> *d, double w) {
+    Edge<T> *e = new Edge<T>(this, d, w);
+    adj.push_back(e);
+    return e;
+}
+
+template <class T>
+bool Vertex<T>::operator<(const Vertex<T> & vertex) const {
+    return this->dist < vertex.dist;
+}
+
+template <class T>
+T Vertex<T>::getInfo() const {
+    return this->info;
+}
+
+template <class T>
+double Vertex<T>::getDist() const {
+    return this->dist;
+}
+
+template <class T>
+int Vertex<T>::getQueueIndex() const {
+    return this->queueIndex;
+}
+
+template <class T>
+Vertex<T> *Vertex<T>::getPath() const {
+    return this->path;
+}
+
+
+// E D G E
+
+template <class T>
+Edge<T>::Edge(Vertex<T> *o, Vertex<T> *d, double w): orig(o), dest(d), weight(w) {}
+
+template <class T>
+double Edge<T>::getWeight() const {
+    return weight;
+}
+
+template <class T>
+Vertex<T> *Edge<T>::getOrig() const {
+    return orig;
+}
+
+template <class T>
+Vertex<T> *Edge<T>::getDest() const {
+    return dest;
+}
+
+
+// G R A P H
+
+template <class T>
+std::vector<Vertex<T> *> Graph<T>::getVertexSet() const {
+    return this->vertexSet;
+}
+
+template <class T>
+Vertex<T>* Graph<T>::addVertex(const T &in) {
+    Vertex<T> *v = findVertex(in);
+    if (v != nullptr)
+        return v;
+    v = new Vertex<T>(in);
+    vertexSet.push_back(v);
+    return v;
+}
+
+template <class T>
+Edge<T>* Graph<T>::addEdge(const T &sourc, const T &dest, double w) {
+    auto s = findVertex(sourc);
+    auto d = findVertex(dest);
+    if (s == nullptr || d == nullptr)
+        return nullptr;
+    Edge<T> *e = new Edge<T>(s, d, w);
+    s->addEdge(e);
+    return e;
+}
+
+template <class T>
+Vertex<T>* Graph<T>::findVertex(const T & inf) const {
+    for (auto v : vertexSet)
+        if (v->info == inf)
+            return v;
+    return nullptr;
+}
+
+template <class T>
+Graph<T>::~Graph() {
+
+    for (auto v : vertexSet) {
+        for (auto e : v->adj)
+            delete e;
+        delete v;
+    }
+
+}
+
+
+// A L G O R I T H M S
+
+template<class T>
+void Graph<T>::floydWarshallShortestPath() { //Makes matrix with all paths
+
+    for (unsigned int i = 0; i < vertexSet.size(); i++)
+        vertexSet[i]->queueIndex = i;
+
+    for (unsigned int i = 0; i < vertexSet.size(); i++) {
+        std::vector<double> temp1;
+        std::vector<int> temp2;
+        for (unsigned int j = 0; j < vertexSet.size(); j++) {
+            temp1.push_back(INF);
+            temp2.push_back(0);
+        }
+        floydWarshallDistanceMatrix.push_back(temp1); //Weights
+        floydWarshallPathsMatrix.push_back(temp2); //Paths
+    }
+
+    for (Vertex<T>* vertex : vertexSet) {
+        for (Edge<T> edge : vertex->adj) { //Initial fill in
+            floydWarshallDistanceMatrix[vertex->queueIndex][edge.dest->queueIndex] = edge.weight;
+            floydWarshallPathsMatrix[vertex->queueIndex][edge.dest->queueIndex] = edge.dest->queueIndex;
+        }
+    }
+
+    for (unsigned int i = 0; i < vertexSet.size(); i++) {
+        for (unsigned int j = 0; j < vertexSet.size(); j++) {
+            for (unsigned int k = 0; k < vertexSet.size(); k++) {
+                if (floydWarshallDistanceMatrix[j][k] > floydWarshallDistanceMatrix[j][i] + floydWarshallDistanceMatrix[i][k]) { //Checks if going through i makes the path shorter
+                    floydWarshallDistanceMatrix[j][k] = floydWarshallDistanceMatrix[j][i] + floydWarshallDistanceMatrix[i][k];
+                    floydWarshallPathsMatrix[j][k] = floydWarshallPathsMatrix[j][i];
+                }
+            }
+        }
+    }
+}
+
--- Entrega2/Algorithms/Graph.h
+++ Entrega2/Algorithms/Graph.h
@@ -6,9 +6,77 @@
 #define ENTREGA2_GRAPH_H
 
 
-class Graph {
 
+#include <vector>
+#include <queue>
+#include <limits>
+#include <algorithm>
+#include <unordered_set>
+#include "MutablePriorityQueue.h"
+
+template <class T> class Edge;
+template <class T> class Graph;
+template <class T> class Vertex;
+
+#define INF std::numeric_limits<double>::max()
+
+
+template <class T>
+class Vertex {
+    T info; //Information of the node
+    std::vector<Edge<T> *> adj; //Outgoing edges
+    bool visited;
+    double dist = 0;
+    Vertex<T> *path = nullptr;
+    int queueIndex = 0; 		// required by MutablePriorityQueue
+    Edge<T> * addEdge(Vertex<T> *dest, double w);
+public:
+    Vertex(T in);
+    bool operator< (const Vertex<T> & vertex) const; // // required by MutablePriorityQueue
+    T getInfo() const;
+    int getQueueIndex() const;
+    double getDist() const;
+    Vertex *getPath() const;
+    friend class Graph<T>;
+    friend class MutablePriorityQueue<Vertex<T>>;
 };
+
+
+
+template <class T>
+class Edge {
+    Vertex<T> *orig; 		    // Origin vertex
+    Vertex<T> * dest;           // Destination vertex
+    double weight;              // Edge weight
+    Edge<T> *reverse = nullptr;
+public:
+    Edge(Vertex<T> *o, Vertex<T> *d, double w);
+    friend class Graph<T>;
+    friend class Vertex<T>;
+
+    double getWeight() const;
+    Vertex<T> *getOrig() const;
+    Vertex<T> *getDest() const;
+};
+
+template <class T>
+class Graph {
+    std::vector<Vertex<T> *> vertexSet;    // Vertexes
+    std::vector<std::vector<double>> floydWarshallDistanceMatrix;
+    std::vector<std::vector<int>> floydWarshallPathsMatrix;
+public:
+    Vertex<T>* findVertex(const T &in) const;
+    Vertex<T>* addVertex(const T &in);
+    Edge<T>* addEdge(const T &sourc, const T &dest, double w);
+    std::vector<Vertex<T> *> getVertexSet() const;
+    ~Graph();
+
+    // Algorithms
+    void floydWarshallShortestPath();
+};
+
+
+
 
 
 #endif //ENTREGA2_GRAPH_H
